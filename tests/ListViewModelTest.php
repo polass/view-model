ef62151cd07b7ee0e484c1a542976e5c6b0f188a
阿部 裕介 <y.abe@salop.co.jp>
2017-08-04 01:54:27 +0900

    ListViewModel のテストを追加

---
+++ tests/ListViewModelTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Polass\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Polass\Fluent\Model;
+
+class ListViewModelTest extends TestCase
+{
+    /**
+     * `toArray()` のテスト
+     *
+     */
+    public function testToArray()
+    {
+        $viewModel = new Stubs\ExtendedListViewModel(collect([ new Model ]), Stubs\ExtendedViewModel::class);
+        $inner = new Stubs\ExtendedViewModel;
+
+        $this->assertEquals(
+            [ $inner->toArrayAnswer ], $viewModel->toArray()->all()
+        );
+    }
+}
